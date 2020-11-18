9b4a29fa9d64a837123051f0585cc3ffd1a8cca0
Ric Evans <emejqz@gmail.com>
2020-11-18 12:21:31 -0600

    change requirements.txt path

--- resources/indexer_env.sh
+++ resources/indexer_env.sh
@@ -2,5 +2,5 @@
 eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/setup.sh`
 python3 -m virtualenv -p python3 env-fc-indexer
 . env-fc-indexer/bin/activate
-pip install -r /tmp/requirements.txt
+pip install -r requirements.txt
 $SROOT/metaprojects/combo/stable/env-shell.sh $@
