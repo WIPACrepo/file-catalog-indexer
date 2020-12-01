78a9e3e635767c68f95081825d5fdb487062ee6f
Ric Evans <emejqz@gmail.com>
2020-11-30 19:05:57 -0600

    add i3 specifics; preserve some substrings

--- resources/filename_pattern_finder.py
+++ resources/filename_pattern_finder.py
@@ -5,7 +5,7 @@ import logging
 import os
 import re
 import subprocess
-from typing import Dict, Set
+from typing import Dict
 
 import yaml
 
@@ -13,6 +13,9 @@ try:
     from typing import TypedDict
 except:  # noqa: E722 # pylint: disable=W0702
     TypedDict = Dict
+
+I3RP = "non-i3-redacted-paths"
+NON_I3RP = f"non-{I3RP}"
 
 
 def get_full_path(path: str) -> str:
@@ -28,21 +31,53 @@ def get_full_path(path: str) -> str:
 
 
 def redact(fpath: str) -> None:
-    """Write out basic patterns; return name of out-file."""
-    with open("redacted.raw", "w") as bpf:
+    """Write out basic patterns."""
+    allowed_substrs = [
+        "i3.bz2",
+        "i3",
+        "level2",
+        "Level2",
+        "level3",
+        "Level3",
+        "level4",
+        "Level4",
+    ]
+    assert len(allowed_substrs) < 32  # there are only 32 non-printable chars
+
+    def _temp_replace(line: str) -> str:
+        for i, char in enumerate(allowed_substrs):
+            line = re.sub(char, chr(i), line)
+        return line
+
+    def _replace_back(line: str) -> str:
+        for i, char in enumerate(allowed_substrs):
+            line = re.sub(chr(i), char, line)
+        return line
+
+    with open(f"{NON_I3RP}.raw", "w") as nonf, open(f"{I3RP}.raw", "w") as i3f:
         with open(fpath, "r") as f:
             for line in f.readlines():
+                # weird file, probably some kind of backup file
                 if "#" in line:
                     logging.warning(f'"#" in filepath: {line.strip()}')
-                # replace each string of digits w/ '#'
-                redacted = re.sub(r"\d+", "#", line)
-                bpf.write(redacted)
+                else:
+                    line = _temp_replace(line)
+                    line = re.sub(r"\d+", "#", line)  # replace strings of digits w/ '#'
+                    line = _replace_back(line)
+                    # .i3 file
+                    if ".i3" in line:
+                        i3f.write(line)
+                    # non-i3 file
+                    else:
+                        nonf.write(line)
 
-    subprocess.check_call("sort redacted.raw > redacted-paths.txt", shell=True)
-    os.remove("redacted.raw")
+    subprocess.check_call(f"sort {NON_I3RP}.raw > {NON_I3RP}", shell=True)
+    os.remove(f"{NON_I3RP}.raw")
+    subprocess.check_call(f"sort {I3RP}.raw > {I3RP}", shell=True)
+    os.remove(f"{I3RP}.raw")
 
 
-def summarize() -> None:
+def summarize(fname: str) -> None:
     """Create a YAML summary with filename patterns."""
 
     class _PatternSummary(TypedDict):
@@ -51,7 +86,7 @@ def summarize() -> None:
 
     summary: Dict[str, _PatternSummary] = {}
 
-    with open("redacted-paths.txt", "r") as f:
+    with open(fname, "r") as f:
         for line in f:
             match = re.match(r"(?P<dpath>.+)/(?P<fname_pattern>[^/]+)$", line.strip())
             if match:
@@ -69,7 +104,7 @@ def summarize() -> None:
             else:
                 logging.info(f"no match: '{line.strip()}'")
 
-    with open("summary.yaml", "w") as f:
+    with open(f"{fname}-summary.yaml", "w") as f:
         yaml.dump(
             dict(sorted(summary.items(), key=lambda ps: ps[1]["count"], reverse=True)),
             f,
@@ -88,16 +123,17 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    if "redacted-paths.txt" in os.listdir("."):
+    if I3RP in os.listdir(".") and NON_I3RP in os.listdir("."):
         logging.info("Using existing redacted-paths.txt")
     elif not args.file:
-        logging.critical("--file or an existing './redacted-paths.txt' is required")
-        raise RuntimeError("--file or an existing './redacted-paths.txt' is required")
+        logging.critical(f"must have './{I3RP}' and './{NON_I3RP}'; OR use --file")
+        raise RuntimeError(f"must have './{I3RP}' and './{NON_I3RP}'; OR use --file")
     else:
         logging.info(f"Parsing {args.file}...")
         redact(args.file)
 
-    summarize()
+    for fname in [I3RP, NON_I3RP]:
+        summarize(fname)
 
 
 if __name__ == "__main__":
