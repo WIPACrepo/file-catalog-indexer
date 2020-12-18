55680ca7c76960d4753bbd83b012a30f8f3d69a2
Ric Evans <emejqz@gmail.com>
2020-12-18 16:24:40 -0600

    regexer: add support for yaml reading

--- resources/filename_patterns/filename_pattern_regexer.py
+++ resources/filename_patterns/filename_pattern_regexer.py
@@ -6,6 +6,9 @@ Use with: grep -P `python filename_pattern_regexer.py <string>` <file>
 
 import logging
 import sys
+from typing import List
+
+import yaml
 
 sys.path.append(".")
 from filename_pattern_finder import (  # isort:skip  # noqa # pylint: disable=C0413
@@ -19,43 +22,58 @@ from filename_pattern_finder import (  # isort:skip  # noqa # pylint: disable=C0
 #
 # Prep
 
-string = sys.argv[1]
-string = string.replace(".", r"\.")
+strings: List[str] = []
 
+if sys.argv[1].endswith(".yaml"):
+    with open(sys.argv[1], "r") as f:
+        in_yaml = yaml.load(f)
+    if isinstance(in_yaml, dict):
+        strings = list(in_yaml.keys())
+    elif isinstance(in_yaml, list):
+        strings = in_yaml
+    elif isinstance(in_yaml, set):
+        strings = list(in_yaml)
+    else:
+        raise Exception(f"Unsupported yaml type: {type(in_yaml)}")
+else:
+    strings = [sys.argv[1]]
 
-#
-# First-stage tokenization
+# Regex-ify!
+for string in strings:
+    string = string.replace(".", r"\.")
 
-string = string.replace("YYYY", r"\d\d\d\d")
-string = string.replace("#", r"\d+")
-string = string.replace("^", r"\d+")
+    #
+    # First-stage tokenization
 
-I3_EXT_REGEX = "(" + "|".join(x.replace(".", r"\.") for x in I3_EXTENSIONS) + ")"
-string = string.replace(I3_EXT_TOKEN.replace(".", r"\."), I3_EXT_REGEX)
+    string = string.replace("YYYY", r"\d\d\d\d")
+    string = string.replace("#", r"\d+")
+    string = string.replace("^", r"\d+")
 
+    I3_EXT_REGEX = "(" + "|".join(x.replace(".", r"\.") for x in I3_EXTENSIONS) + ")"
+    string = string.replace(I3_EXT_TOKEN.replace(".", r"\."), I3_EXT_REGEX)
 
-#
-# Second-stage tokenization
+    #
+    # Second-stage tokenization
 
-for special_num_string in SPECIAL_NUM_STRINGS:
-    string = string.replace(
-        special_num_string["token"], special_num_string["normal_regex"]
+    for special_num_string in SPECIAL_NUM_STRINGS:
+        string = string.replace(
+            special_num_string["token"],
+            f"(?P<{special_num_string['token'].lower()}>{special_num_string['normal_regex']})",
+        )
+
+    SPECIAL_SUFFIXES_REGEX = (
+        "(" + "|".join(x.replace(".", r"\.") for x in SPECIAL_SUFFIXES) + ").*"
     )
+    string = string.replace("SUFFIX", SPECIAL_SUFFIXES_REGEX)
 
-SPECIAL_SUFFIXES_REGEX = (
-    "(" + "|".join(x.replace(".", r"\.") for x in SPECIAL_SUFFIXES) + ").*"
-)
-string = string.replace("SUFFIX", SPECIAL_SUFFIXES_REGEX)
+    for num_sequence in NUM_SEQUENCES:
+        string = string.replace(
+            num_sequence["token"],
+            f"(?P<{num_sequence['token'].lower()}>{num_sequence['normal_regex']})",
+        )
 
-for num_sequence in NUM_SEQUENCES:
-    string = string.replace(
-        num_sequence["token"],
-        f"(?P<{num_sequence['token'].lower()}>{num_sequence['normal_regex']})",
-    )
+    #
+    # Print
 
-
-#
-# Print
-
-logging.info(string)
-print(string)
+    logging.info(string)
+    print(string)
