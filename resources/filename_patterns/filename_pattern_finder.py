eb26aefca8d87b565b4a6691019665fbc3a21338
Ric Evans <emejqz@gmail.com>
2020-12-18 10:40:18 -0600

    split INDIACHARLIEYANKEEFOURNUM

--- resources/filename_patterns/filename_pattern_finder.py
+++ resources/filename_patterns/filename_pattern_finder.py
@@ -252,10 +252,16 @@ SPECIAL_NUM_STRINGS: List[_SpecialNumStrings] = [
         "normal_regex": r"KYG1_distr_\d+",
     },
     {
-        "find": "YYYY",
-        "hash_regex": r"IC(\^\.)+YYYY",
+        "find": "ICYYYY",
+        "hash_regex": r"ICYYYY",
         "num_token": "INDIACHARLIEYANKEEFOURNUM",  # phonetic alphabet
-        "normal_regex": r"IC(\d+\.)+\d\d\d\d",
+        "normal_regex": r"IC\d\d\d\d",
+    },
+    {
+        "find": "YYYY",
+        "hash_regex": r"IC\^\.YYYY",
+        "num_token": "INDIACHARLIENUMYANKEEFOURNUM",  # phonetic alphabet
+        "normal_regex": r"IC\d+\.\d\d\d\d",
     },
 ]
 # must use good 'num_token's
