425f97977074d6a30c2fd7913ca6655ac5f18219
Ric Evans <emejqz@gmail.com>
2021-01-28 10:16:18 -0600

    move tests/integration/real test data to data.py

---
+++ tests/integration/real/data.py
@@ -0,0 +1,157 @@
+"""Example file metadata."""
+
+EXAMPLES = {
+    "PFRaw/PFRaw_PhysicsTrig_PhysicsFiltering_Run00109298_Subrun00000000_00000064.tar.gz": {
+        "_links": {
+            "parent": {"href": "/api/files"},
+            "self": {"href": "/api/files/4a59b52c-7a46-11ea-911f-3a952b566ed1"},
+        },
+        "checksum": {
+            "sha512": "0880e00e70d72f9d7ce1d6905cf874b36b63f0ae8acc71d53baf0628c72089985204d1baadcc9432ee5c09e387dffd9741a6257ae5dcdfbc710b90b5262e5a67"
+        },
+        "content_status": "good",
+        "create_date": "2007-09-11",
+        "data_type": "real",
+        "file_size": 623743809,
+        "locations": [
+            {
+                "site": "WIPAC",
+                "path": "/data/exp/IceCube/2007new/filtered/GRB/0911/PFRaw_PhysicsTrig_PhysicsFiltering_Run00109298_Subrun00000000_00000064.tar.gz",
+            }
+        ],
+        "logical_name": "/data/exp/IceCube/2007new/filtered/GRB/0911/PFRaw_PhysicsTrig_PhysicsFiltering_Run00109298_Subrun00000000_00000064.tar.gz",
+        "meta_modify_date": "2020-05-26 20:04:56.831678",
+        "processing_level": "PFRaw",
+        "run": {
+            "end_datetime": "2007-09-11T06:11:08",
+            "event_count": 158659,
+            "first_event": 10127717,
+            "last_event": 10286375,
+            "part_number": 64,
+            "run_number": 109298,
+            "start_datetime": "2007-09-11T06:06:45",
+            "subrun_number": 0,
+        },
+        "software": [
+            {
+                "name": "http://code.icecube.wisc.edu/svn/meta-projects/jeb/releases/V00-00-07",
+                "version": "35754",
+            }
+        ],
+        "uuid": "4a59b52c-7a46-11ea-911f-3a952b566ed1",
+    },
+    "PFFilt/PFFilt_PhysicsTrig_PhysicsFilt_Run00087385_00008.tar.gz": {
+        "_links": {
+            "parent": {"href": "/api/files"},
+            "self": {"href": "/api/files/8c85ec48-7a31-11ea-9053-3a952b566ed1"},
+        },
+        "checksum": {
+            "sha512": "27f87b5a62721df5b39d9af19db447367871615ae8cc1ef5253668b8feeee278b4607630c45a566df7f3bf85429f06004e255015a87bb6e50d2d3a25c416bcb9"
+        },
+        "content_status": "good",
+        "create_date": "2006-04-17",
+        "data_type": "real",
+        "file_size": 1505807,
+        "locations": [
+            {
+                "site": "WIPAC",
+                "path": "/data/exp/IceCube/2006/filtered/PFFilt/0417/PFFilt_PhysicsTrig_PhysicsFilt_Run00087385_00008.tar.gz",
+            }
+        ],
+        "logical_name": "/data/exp/IceCube/2006/filtered/PFFilt/0417/PFFilt_PhysicsTrig_PhysicsFilt_Run00087385_00008.tar.gz",
+        "meta_modify_date": "2020-04-09 07:13:01.779505",
+        "processing_level": "PFFilt",
+        "run": {
+            "end_datetime": "2006-04-17T15:11:50",
+            "event_count": 0,
+            "first_event": None,
+            "last_event": None,
+            "part_number": 8,
+            "run_number": 87385,
+            "start_datetime": "2006-04-17T15:10:14",
+            "subrun_number": 0,
+        },
+        "software": [{"name": "PhysicsFilt", "version": "01.00.00"}],
+        "uuid": "8c85ec48-7a31-11ea-9053-3a952b566ed1",
+    },
+    "PFDST/PFDST_PhysicsTrig_PhysicsFiltering_Run00116892_Subrun00000000_00000134.tar.gz": {
+        "_links": {
+            "parent": {"href": "/api/files"},
+            "self": {"href": "/api/files/87663bc2-7b1f-11ea-9e1d-3a952b566ed1"},
+        },
+        "checksum": {
+            "sha512": "86fec738daab38774e81a70bdb93675cb53aaf42836ecb19620c4c814f16c53fcea09997b173d0c7c8f2c79072f77ec814e9f6102bd4de6064d3f66e767e11ce"
+        },
+        "content_status": "good",
+        "create_date": "2014-04-30T07:30:09",
+        "data_type": "real",
+        "file_size": 286937687,
+        "locations": [
+            {
+                "site": "WIPAC",
+                "path": "/data/exp/IceCube/2010/unbiased/PFDST/1116/PFDST_PhysicsTrig_PhysicsFiltering_Run00116892_Subrun00000000_00000134.tar.gz",
+            }
+        ],
+        "logical_name": "/data/exp/IceCube/2010/unbiased/PFDST/1116/PFDST_PhysicsTrig_PhysicsFiltering_Run00116892_Subrun00000000_00000134.tar.gz",
+        "meta_modify_date": "2020-04-10 11:36:33.404894",
+        "processing_level": "PFDST",
+        "run": {
+            "end_datetime": "2010-11-16T19:17:34.6730371",
+            "event_count": 407730,
+            "first_event": 54644725,
+            "last_event": 55052454,
+            "part_number": 134,
+            "run_number": 116892,
+            "start_datetime": "2010-11-16T19:14:40.3904744109",
+            "subrun_number": 0,
+        },
+        "software": [
+            {"name": "filterscripts", "version": "V15-05-01"},
+            {"name": "icerec", "version": "IC2011-L2_V12-08-00"},
+        ],
+        "uuid": "87663bc2-7b1f-11ea-9e1d-3a952b566ed1",
+    },
+    "L2/Level2_IC86.2012_data_Run00120243_Subrun00000311.i3.bz2": {
+        "_links": {
+            "parent": {"href": "/api/files"},
+            "self": {"href": "/api/files/387b924a-79f1-11ea-a3bc-3a952b566ed1"},
+        },
+        "checksum": {
+            "sha512": "7e8746cfe3ea3078f13fd11d037bbadeae2716291279e93838fb84a40870d9f4ed06dd06b45018e6bcee86367261637fe89b3b9b4f595380d93da05493f26223"
+        },
+        "content_status": "good",
+        "create_date": "2018-07-31",
+        "data_type": "real",
+        "file_size": 4463,
+        "locations": [
+            {
+                "site": "WIPAC",
+                "path": "/data/exp/DM-Ice/2012/filtered/pole/icecube_coincidence/06/det1/Level2_IC86.2012_data_Run00120243_Subrun00000311.i3.bz2",
+            }
+        ],
+        "logical_name": "/data/exp/DM-Ice/2012/filtered/pole/icecube_coincidence/06/det1/Level2_IC86.2012_data_Run00120243_Subrun00000311.i3.bz2",
+        "meta_modify_date": "2020-04-08 23:32:32.992368",
+        "offline_processing_metadata": {
+            "L2_gcd_file": "",
+            "first_event": None,
+            "gaps": None,
+            "last_event": None,
+            "livetime": None,
+            "season": 2012,
+            "season_name": "IC86-2",
+        },
+        "processing_level": "L2",
+        "run": {
+            "end_datetime": None,
+            "event_count": 2,
+            "first_event": 71834853,
+            "last_event": 71834853,
+            "part_number": 311,
+            "run_number": 120243,
+            "start_datetime": None,
+            "subrun_number": 0,
+        },
+        "software": None,
+        "uuid": "387b924a-79f1-11ea-a3bc-3a952b566ed1",
+    },
+}
--- tests/integration/real/test_data_exp_metadata.py
+++ tests/integration/real/test_data_exp_metadata.py
@@ -4,26 +4,18 @@
 2. metadata_file = manager.new_file(filepath)
 3. metadata = metadata_file.generate()
 """
+import sys
 
-EXAMPLES = {
-    "PFRaw": {
-        "logical_name": "/data/exp/IceCube/2007new/filtered/GRB/0911/"
-        "PFRaw_PhysicsTrig_PhysicsFiltering_Run00109298_Subrun00000000_00000064.tar.gz",
-        "uuid": "4a59b52c-7a46-11ea-911f-3a952b566ed1",
-    },
-    "PFFilt": {
-        "logical_name": "/data/exp/IceCube/2006/filtered/PFFilt/0417/"
-        "PFFilt_PhysicsTrig_PhysicsFilt_Run00087385_00008.tar.gz",
-        "uuid": "8c85ec48-7a31-11ea-9053-3a952b566ed1",
-    },
-    "PFDST": {
-        "logical_name": "/data/exp/IceCube/2010/unbiased/PFDST/1116/"
-        "PFDST_PhysicsTrig_PhysicsFiltering_Run00116892_Subrun00000000_00000134.tar.gz",
-        "uuid": "87663bc2-7b1f-11ea-9e1d-3a952b566ed1",
-    },
-    "L2": {
-        "logical_name": "/data/exp/DM-Ice/2012/filtered/pole/icecube_coincidence/06/det1/"
-        "Level2_IC86.2012_data_Run00120243_Subrun00000311.i3.bz2",
-        "uuid": "387b924a-79f1-11ea-a3bc-3a952b566ed1",
-    },
-}
+# local imports
+import data
+
+sys.path.append(".")
+from indexer import metadata_manager  # isort:skip # noqa # pylint: disable=C0413
+
+
+def test_1() -> None:
+    """Test all example passing cases."""
+    for fpath, metadata in data.EXAMPLES.items():
+        manager = metadata_manager.MetadataManager("WIPAC")
+        metadata_file = manager.new_file(fpath)
+        assert metadata == metadata_file.generate()
