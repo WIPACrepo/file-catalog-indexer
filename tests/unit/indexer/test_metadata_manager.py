c52eb5c0ae88ccbd37f95ed56c4e8ac9c97e38d3
Ric Evans <emejqz@gmail.com>
2021-01-29 18:49:42 -0600

    add unit test for MetadataManager.new_file's initial factory logic

---
+++ tests/unit/indexer/test_metadata_manager.py
@@ -0,0 +1,44 @@
+"""Selectively unit test metadata_mangager.py."""
+
+# pylint: disable=W0212
+
+import sys
+
+sys.path.append(".")
+from indexer import metadata_manager  # isort:skip # noqa # pylint: disable=C0413
+
+
+def test_new_file_logic() -> None:
+    """Test MetadataManager.new_file()'s factory logic."""
+    # good - /data/sim/*
+    assert metadata_manager.MetadataManager._is_data_sim_filepath("/data/sim/foo")
+    assert not metadata_manager.MetadataManager._is_data_exp_filepath("/data/sim/foo")
+
+    # good - /data/exp/*
+    assert not metadata_manager.MetadataManager._is_data_sim_filepath("/data/exp/foo")
+    assert metadata_manager.MetadataManager._is_data_exp_filepath("/data/exp/foo")
+
+    # bad - */data/sim/*
+    assert not metadata_manager.MetadataManager._is_data_sim_filepath("~/data/sim/bar")
+    assert not metadata_manager.MetadataManager._is_data_exp_filepath("~/data/sim/bar")
+
+    # bad - */data/sim/*
+    assert not metadata_manager.MetadataManager._is_data_sim_filepath("../exp/sim/bar")
+    assert not metadata_manager.MetadataManager._is_data_exp_filepath("../exp/sim/bar")
+
+    # bad - /mnt/*
+    assert not metadata_manager.MetadataManager._is_data_sim_filepath("/mnt/lfs6/sim/b")
+    assert not metadata_manager.MetadataManager._is_data_exp_filepath("/mnt/lfs6/sim/b")
+    assert not metadata_manager.MetadataManager._is_data_sim_filepath("/mnt/lfs6/exp/b")
+    assert not metadata_manager.MetadataManager._is_data_exp_filepath("/mnt/lfs6/exp/b")
+
+    # bad - misc
+    assert not metadata_manager.MetadataManager._is_data_sim_filepath("//data/sim/foo")
+    assert not metadata_manager.MetadataManager._is_data_exp_filepath("//data/sim/foo")
+    assert not metadata_manager.MetadataManager._is_data_sim_filepath("//data/exp/foo")
+    assert not metadata_manager.MetadataManager._is_data_exp_filepath("//data/exp/foo")
+    #
+    assert not metadata_manager.MetadataManager._is_data_sim_filepath("/data/simu/a")
+    assert not metadata_manager.MetadataManager._is_data_exp_filepath("/data/simu/a")
+    assert not metadata_manager.MetadataManager._is_data_sim_filepath("/data/expo/b")
+    assert not metadata_manager.MetadataManager._is_data_exp_filepath("/data/expo/b")
