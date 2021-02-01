ee3c59d2f06198d89809f0ad1ebb6dc2028bc600
Ric Evans <emejqz@gmail.com>
2021-02-01 14:02:44 -0600

    mock _get_events_data()

--- tests/integration/simulation/test_data_sim_metadata.py
+++ tests/integration/simulation/test_data_sim_metadata.py
@@ -21,7 +21,10 @@ SKIP_FIELDS = ["_links", "meta_modify_date", "uuid"]
 
 @patch("indexer.metadata_manager.MetadataManager._is_data_sim_filepath")
 @patch("indexer.metadata_manager.MetadataManager._is_data_exp_filepath")
-def test_1(_is_data_exp_filepath: Mock, _is_data_sim_filepath: Mock,) -> None:
+@patch("indexer.metadata.i3.I3FileMetadata._get_events_data")
+def test_1(
+    _is_data_exp_filepath: Mock, _is_data_sim_filepath: Mock, _get_events_data: Mock
+) -> None:
     """Test all example passing cases."""
     for fpath, metadata in data.EXAMPLES.items():
         print(fpath)
@@ -36,6 +39,14 @@ def test_1(_is_data_exp_filepath: Mock, _is_data_sim_filepath: Mock,) -> None:
         # mock MetadataManager.new_file's initial factory logic
         _is_data_sim_filepath.return_value = True
         _is_data_exp_filepath.return_value = False
+        # mock I3Reader-dependent method
+        dummy_event_data = {
+            "first_event": metadata["run"]["first_event"],
+            "last_event": metadata["run"]["last_event"],
+            "event_count": metadata["run"]["event_count"],
+            "status": metadata["content_status"],
+        }
+        _get_events_data.return_value = dummy_event_data
 
         # run
         manager = metadata_manager.MetadataManager(  # TODO think about mocking the rest/db
