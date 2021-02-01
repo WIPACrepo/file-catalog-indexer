8d84d2500595674c1c2ebd32435a798abe3fac09
Ric Evans <emejqz@gmail.com>
2021-02-01 12:39:34 -0600

    add /data/sim/ integration testing files & initial tests

---
+++ tests/integration/simulation/data.py
@@ -0,0 +1,101 @@
+"""Example file metadata."""
+
+from typing import Any, Dict
+
+EXAMPLES: Dict[str, Dict[str, Any]] = {
+    "Level2_IC86.2011_corsika.010285.000000.i3.bz2": {
+        "checksum": {
+            "sha512": "f246b07132968374ded4627b2fc93c8af5a77468ced5b90327fe8ac00685e6ff549a61daac37a3fbbdf51a1a8add35cb10ed8d402ef89eaf1ed4c272da150331"
+        },
+        "content_status": "good",
+        "create_date": "2010-02-01",
+        "data_type": "simulation",
+        "file_size": 30,
+        "iceprod": {},  # TODO
+        "simulation": {},  # TODO
+        "locations": [
+            {
+                "site": "WIPAC",
+                "path": "/data/sim/IceCube/2011/filtered/level2/CORSIKA-in-ice/10285/00000-00999/Level2_IC86.2011_corsika.010285.000000.i3.bz2",
+            }
+        ],
+        "logical_name": "/data/sim/IceCube/2011/filtered/level2/CORSIKA-in-ice/10285/00000-00999/Level2_IC86.2011_corsika.010285.000000.i3.bz2",
+        "processing_level": "L2",
+    },
+    "TopSimulator_IC86_corsika_icetop.010410.000001.i3.bz2": {
+        "checksum": {
+            "sha512": "d54f662f21df9bfa5e697731ebc31aeff7c7aa77fc929fb21cf1d8ee027e4b89bb6540b2f4829abcb3466e233b39e066bb90927b97d490076ec986ce96f2e942"
+        },
+        "content_status": "good",
+        "create_date": "2010-02-01",
+        "data_type": "simulation",
+        "file_size": 35,
+        "iceprod": {},  # TODO
+        "simulation": {},  # TODO
+        "locations": [
+            {
+                "site": "WIPAC",
+                "path": "/data/sim/IceCube/2011/generated/CORSIKA-ice-top/12333/topsimulator/0000000-0000999/TopSimulator_IC86_corsika_icetop.010410.000001.i3.bz2",
+            }
+        ],
+        "logical_name": "/data/sim/IceCube/2011/generated/CORSIKA-ice-top/12333/topsimulator/0000000-0000999/TopSimulator_IC86_corsika_icetop.010410.000001.i3.bz2",
+        "processing_level": "Generated",
+    },
+    "hits.020900.000000.i3.zst": {
+        "checksum": {
+            "sha512": "f5babdb6edf51d4b4b19acac1c296518f30c0a8664213e589a2675ce9d64f634493f7dba05bb32e211732da466b168162cb8c45bd74676f0055e403df88922fb"
+        },
+        "content_status": "good",
+        "create_date": "2010-02-01",
+        "data_type": "simulation",
+        "file_size": 32,
+        "iceprod": {},  # TODO
+        "simulation": {},  # TODO
+        "locations": [
+            {
+                "site": "WIPAC",
+                "path": "/data/sim/IceCube/2016/generated/CORSIKA-in-ice/20900/0000000-0000999/hits.020900.000000.i3.zst",
+            }
+        ],
+        "logical_name": "/data/sim/IceCube/2016/generated/CORSIKA-in-ice/20900/0000000-0000999/hits.020900.000000.i3.zst",
+        "processing_level": "Propagated",
+    },
+    "genie_ic.60008.000000.L3.i3.gz": {
+        "checksum": {
+            "sha512": "939b4fb2be4759d9550156c2b07574a9d64c42d2ae884e0f44289be1781d4b6f0b706b9933879aaadc192278284a1564153eb2459990cd3397335b25f9f8b5e9"
+        },
+        "content_status": "good",
+        "create_date": "2010-02-01",
+        "data_type": "simulation",
+        "file_size": 30,
+        "iceprod": {},  # TODO
+        "simulation": {},  # TODO
+        "locations": [
+            {
+                "site": "WIPAC",
+                "path": "/data/sim/IceCube/2011/generated/GENIE-in-ice/PINGU/numu/60008/genie_ic.60008.000000.L3.i3.gz",
+            }
+        ],
+        "logical_name": "/data/sim/IceCube/2011/generated/GENIE-in-ice/PINGU/numu/60008/genie_ic.60008.000000.L3.i3.gz",
+        "processing_level": "L3",
+    },
+    "Detector_IC86_corsika_icetop.010410.000001.i3.bz2": {
+        "checksum": {
+            "sha512": "24cc25ee89f8a3669b4f48be58e806ab9ce01fd185a991a2888a464fa9f6c36cda2eb0fdcf0f6213ae68b54008cf756159c443cc96b1c55ca9cac1142e260093"
+        },
+        "content_status": "good",
+        "create_date": "2010-02-01",
+        "data_type": "simulation",
+        "file_size": 34,
+        "iceprod": {},  # TODO
+        "simulation": {},  # TODO
+        "locations": [
+            {
+                "site": "WIPAC",
+                "path": "/data/sim/IceCube/2011/generated/CORSIKA-ice-top/12333/detector/0000000-0000999/Detector_IC86_corsika_icetop.010410.000001.i3.bz2",
+            }
+        ],
+        "logical_name": "/data/sim/IceCube/2011/generated/CORSIKA-ice-top/12333/detector/0000000-0000999/Detector_IC86_corsika_icetop.010410.000001.i3.bz2",
+        "processing_level": "Triggered",
+    },
+}
--- tests/integration/simulation/test_data_sim_metadata.py
+++ tests/integration/simulation/test_data_sim_metadata.py
@@ -4,3 +4,51 @@
 2. metadata_file = manager.new_file(filepath)
 3. metadata = metadata_file.generate()
 """
+
+import os
+import sys
+from unittest.mock import Mock, patch
+
+# local imports
+import data
+
+sys.path.append(".")
+from indexer import metadata_manager  # isort:skip # noqa # pylint: disable=C0413
+
+
+SKIP_FIELDS = ["_links", "meta_modify_date", "uuid"]
+
+
+@patch("indexer.metadata_manager.MetadataManager._is_data_sim_filepath")
+@patch("indexer.metadata_manager.MetadataManager._is_data_exp_filepath")
+def test_1(_is_data_exp_filepath: Mock, _is_data_sim_filepath: Mock,) -> None:
+    """Test all example passing cases."""
+    for fpath, metadata in data.EXAMPLES.items():
+        print(fpath)
+
+        # prep
+        fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), fpath)
+        metadata["logical_name"] = fullpath
+        metadata["locations"] = [
+            {"site": metadata["locations"][0]["site"], "path": fullpath}
+        ]
+
+        # mock MetadataManager.new_file's initial factory logic
+        _is_data_sim_filepath.return_value = True
+        _is_data_exp_filepath.return_value = False
+
+        # run
+        manager = metadata_manager.MetadataManager(  # TODO think about mocking the rest/db
+            "WIPAC",
+            iceprodv2_rc_token=open("fc.token").read().strip(),
+            iceprodv1_db_pass=open("ipdb.pass").read().strip(),
+        )
+        metadata_file = manager.new_file(fullpath)
+        generated_metadata = metadata_file.generate()
+
+        # assert
+        for field in metadata:
+            if field in SKIP_FIELDS:
+                continue
+            print(field)
+            assert metadata[field] == generated_metadata[field]  # type: ignore[misc]
