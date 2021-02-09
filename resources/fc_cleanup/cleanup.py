955e2705f07c384b886bc46c7eb2ba16251602ee
Ric Evans <emejqz@gmail.com>
2021-02-08 18:45:40 -0600

    flip script logic--now script finds all "evil twins" by querying FC

--- resources/fc_cleanup/cleanup.py
+++ resources/fc_cleanup/cleanup.py
@@ -1,13 +1,12 @@
 """Cleanup duplicate File Catalog records...
 
-where the duplicate is indexed under the file's realpath.
+where the duplicate is indexed under /mnt/lfs*/.
 """
 
 
 import argparse
 import json
 import logging
-import os
 from typing import Any, cast, Dict, Generator, Tuple
 
 import coloredlogs  # type: ignore[import]
@@ -17,10 +16,10 @@ from rest_tools.client import RestClient  # type: ignore[import]
 
 BATCH_SIZE = 10000
 
-FCFile = Dict[str, Any]
+FCEntry = Dict[str, Any]
 
 
-def _get_fcfile(rc: RestClient, logical_name: str) -> FCFile:
+def _find_fc_entry(rc: RestClient, logical_name: str) -> FCEntry:
 
     # get uuid
     try:
@@ -36,51 +35,72 @@ def _get_fcfile(rc: RestClient, logical_name: str) -> FCFile:
     # get full metadata
     results = rc.request_seq("GET", f"/api/files/{results[0]['uuid']}")
 
-    return cast(FCFile, results[0])
+    return cast(FCEntry, results[0])
 
 
-def get_evil_twin(rc: RestClient, this: FCFile) -> FCFile:
-    """Get the FC file that is indexed under `this`'s realpath."""
-    # realpath() doesn't check if file exists, which is a good b/c file might've moved
-    realpath = os.path.realpath(this["logical_name"])
+def _get_good_path(bad_path: str) -> str:
+    def strip_prefix(string: str, prefix: str) -> str:
+        return string[len(prefix) :]
 
-    evil_twin = _get_fcfile(rc, realpath)
+    if bad_path.startswith("/mnt/lfs6/exp/"):
+        return "/data/exp/" + strip_prefix(bad_path, "/mnt/lfs6/exp/")
+
+    elif bad_path.startswith("/mnt/lfs6/sim/"):
+        return "/data/sim/" + strip_prefix(bad_path, "/mnt/lfs6/exp/")
+
+    raise Exception(f"Unaccounted for prefix: {bad_path}")
+
+
+def find_twins(rc: RestClient, bad_rooted_fpath: str) -> Tuple[FCEntry, FCEntry]:
+    """Get the FC entry that is indexed under `evil_twin`'s canonical path."""
+    # first, try to find entry w/ good path (otherwise, no point to continue)
+    good_twin = _find_fc_entry(rc, _get_good_path(bad_rooted_fpath))
+    evil_twin = _find_fc_entry(rc, bad_rooted_fpath)
 
     # ignore keys that aren't expected to match
     # TODO - what other fields to ignore?
     ignored_fields = ["_links", "logical_name", "uuid"]
 
-    def copy_for_compare(fcfile: FCFile) -> FCFile:
-        xerox = fcfile.copy()
+    def copy_for_compare(entry: FCEntry) -> FCEntry:
+        xerox = entry.copy()
         for field in ignored_fields:
             del xerox[field]
         # replace logical_name in locations object with a placeholder value
         for i, locus in enumerate(xerox["locations"].copy()):
-            if locus["path"] == fcfile["logical_name"]:
+            if locus["path"] == entry["logical_name"]:
                 xerox["locations"][i]["path"] = "PLACEHOLDER"
         return xerox
 
-    if copy_for_compare(this) != copy_for_compare(evil_twin):
+    if copy_for_compare(evil_twin) != copy_for_compare(good_twin):
         raise Exception(
-            f"These don't match {this} vs {evil_twin} (disregarding: {ignored_fields})"
+            f"These don't match {evil_twin} vs {good_twin} (disregarding: {ignored_fields})"
         )
 
-    return evil_twin
+    return evil_twin, good_twin
 
 
-def get_fcfile_and_evil_twin(
-    fpaths_dump: str, rc: RestClient
-) -> Generator[Tuple[FCFile, FCFile], None, None]:
-    """Yield each file and its evil twin (rooted at /mnt/)."""
-    with open(fpaths_dump) as f_dump:
-        for line in f_dump:
-            logical_name = line.strip()
-            fcfile = _get_fcfile(rc, logical_name)
-            try:
-                evil_twin = get_evil_twin(rc, fcfile)
-            except FileNotFoundError:
-                continue
-            yield fcfile, evil_twin
+def bad_rooted_fc_fpaths(rc: RestClient) -> Generator[str, None, None]:
+    """Yield each FC filepath rooted at /mnt/lfs*/."""
+    files = rc.request_seq("GET", "/api/files")["files"]
+
+    bads = [
+        f["logical_name"] for f in files if f["logical_name"].startswith("/mnt/lfs")
+    ]
+
+    yield from bads
+
+
+def get_evil_twin_catalog_entries(rc: RestClient) -> Generator[FCEntry, None, None]:
+    """Yield each FC file and its good twin (rooted at /data/)."""
+    for bad_rooted_fpath in bad_rooted_fc_fpaths(rc):
+        try:
+            evil_twin, good_twin = find_twins(rc, bad_rooted_fpath)
+            logging.info(
+                f'Found {evil_twin["logical_name"]} and {good_twin["logical_name"]}'
+            )
+        except FileNotFoundError:
+            continue
+        yield evil_twin
 
 
 def main() -> None:
@@ -92,24 +112,19 @@ def main() -> None:
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("--token", required=True, help="file catalog token")
-    parser.add_argument(
-        "--files-to-check",
-        required=True,
-        help=r"file containing filepaths to check; each line is: `<logical_name>\n`",
-    )
     args = parser.parse_args()
 
     rc = RestClient("https://file-catalog.icecube.wisc.edu/", token=args.token)
 
     # Go
-    total = 0
-    for fcfile, evil_twin in get_fcfile_and_evil_twin(args.files_to_check, rc):
-        logging.info(f'Found {fcfile["logical_name"]} and {evil_twin["logical_name"]}')
-        # delete evil twin
+    deleted = False
+    for i, evil_twin in enumerate(get_evil_twin_catalog_entries(rc)):
+        deleted = True
         rc.request_seq("DELETE", f"/api/files/{evil_twin['uuid']}")
-        total += 1
+        logging.info(f"Deleted: {i}")
 
-    logging.info(f"Total deleted: {total}")
+    if not deleted:
+        raise Exception("No FC entries found/deleted")
 
 
 if __name__ == "__main__":
