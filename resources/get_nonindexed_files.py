ecad146f49c5ccdab6b2713d69638289fcbd4287
Ric Evans <emejqz@gmail.com>
2021-03-22 16:31:32 -0500

    multi-thread

--- resources/get_nonindexed_files.py
+++ resources/get_nonindexed_files.py
@@ -2,44 +2,75 @@
 
 
 import argparse
+import concurrent.futures
 import logging
+from typing import List
 
 import coloredlogs  # type: ignore[import]
+import more_itertools as mit  # type: ignore[import]
 from rest_tools.client import RestClient  # type: ignore[import]
 
-# args
-parser = argparse.ArgumentParser(
-    description="Submit HTCondor DAGMan jobs for bulk indexing files for the File Catalog",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-)
-parser.add_argument(
-    "--traverse-file",
-    required=True,
-    help="traverse file containing superset of filepaths",
-)
-parser.add_argument("-l", "--log", default="DEBUG", help="the output logging level")
-parser.add_argument("-t", "--token", required=True, help="REST token for File Catalog")
-args = parser.parse_args()
 
-# logging
-args = parser.parse_args()
-coloredlogs.install(level=args.log.upper())
-for arg, val in vars(args).items():
-    logging.warning(f"{arg}: {val}")
+def _check_fpaths(fpaths: List[str], token: str, thread_id: int) -> None:
+    # setup
+    rc = RestClient("https://file-catalog.icecube.wisc.edu/", token=token)
 
-# setup
-rc = RestClient("https://file-catalog.icecube.wisc.edu/", token=args.token)
+    # scan
+    non_indexed = f"{thread_id}.nonindexed.paths"
+    with open(non_indexed, "w") as nonindexed_f:
+        for i, fpath in enumerate(fpaths, start=1):
+            logging.info(f"#{i}")
+            logging.debug(f"Looking at {fpath}")
+            result = rc.request_seq("GET", "/api/files", {"path": fpath})["files"]
+            if result:
+                logging.debug("file is already indexed")
+                continue
+            logging.info(f"file is *not* indexed -> appending to {non_indexed}")
+            print(fpath, file=nonindexed_f)
 
-# scan
-NON_INDEXED = "nonindexed.paths"
-with open(args.traverse_file) as f, open(NON_INDEXED, "w") as nonindexed_f:
-    for i, line in enumerate(f, start=1):
-        logging.info(f"#{i}")
-        fpath = line.strip()
-        logging.debug(f"Looking at {fpath}")
-        result = rc.request_seq("GET", "/api/files", {"path": fpath})["files"]
-        if result:
-            logging.debug("file is already indexed")
-            continue
-        logging.info(f"file is *not* indexed -> appending to {NON_INDEXED}")
-        print(fpath, file=nonindexed_f)
+
+def _split_up_infile(trav_file: str, npieces: int) -> List[List[str]]:
+    fpaths = [ln.strip() for ln in open(trav_file)]
+
+    return [list(c) for c in mit.divide(npieces, fpaths)]
+
+
+def main() -> None:
+    """Do main."""
+    # args
+    parser = argparse.ArgumentParser(
+        description="Get files that have not been indexed from a traverse file.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--traverse-file",
+        required=True,
+        help="traverse file containing superset of filepaths",
+    )
+    parser.add_argument("-l", "--log", default="DEBUG", help="the output logging level")
+    parser.add_argument(
+        "-t", "--token", required=True, help="REST token for File Catalog"
+    )
+    parser.add_argument("--threads", required=True, type=int, help="# of threads")
+    args = parser.parse_args()
+
+    # logging
+    args = parser.parse_args()
+    coloredlogs.install(level=args.log.upper())
+    for arg, val in vars(args).items():
+        logging.warning(f"{arg}: {val}")
+
+    # split up in-file
+    fpath_chunks = _split_up_infile(args.traverse_file, args.threads)
+
+    # spawn threads
+    file_workers: List[concurrent.futures.Future] = []  # type: ignore[type-arg]
+    with concurrent.futures.ProcessPoolExecutor(max_workers=args.threads) as pool:
+        file_workers.extend(
+            pool.submit(_check_fpaths, c, args.token, i)
+            for i, c in enumerate(fpath_chunks)
+        )
+
+
+if __name__ == "__main__":
+    main()
